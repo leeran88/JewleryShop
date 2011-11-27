a9f14ad05971646155359fdd1a0a17b885795e32
leeran88 <Leeran@Leeran-laptop.(none)>
2011-11-27 23:29:27 +0200

    Adding JewleryShop .sln and initial site.

---
+++ Source/JewleryShop/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JewleryShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "הילה - עיצוב תכשיטים. ברוכים הבאים";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
